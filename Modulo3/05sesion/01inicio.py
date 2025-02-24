5959d4392bc4f69772a9efc5b87d6a40afdd828e
Cristhian G. <kris.gar.jim@gmail.com>
2025-02-24 16:54:37 -0500

    commit 24/02/25

---
+++ Modulo3/05sesion/01inicio.py
@@ -0,0 +1,5 @@
+# CLASE PRUEBAS UNITARIAS CON UNITTEST Y PYTEST _ 05/FEB/2025
+
+# CONFIGURAR ENTORNO DE PRUEBAS -> ARCHIVO/PREFERENCIAS/CONFIGURACION -> BUSCAR: python:testing
+# HABILITAR -> Python -> Testing: Pytest Enabled (habilitar casilla)
+# HABILITAR -> Python -> Testing: Unittest Enabled (habilitar casilla)
